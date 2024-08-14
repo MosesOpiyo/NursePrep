fe33e368fd05ccc73256685bf5e1ff48c7d80fc1
unknown <githinjipete21@gmail.com>
2024-08-14 14:50:02 +0300

    remove redundant code in layout

--- src/app/teas/[subjectId]/layout.tsx
+++ src/app/teas/[subjectId]/layout.tsx
@@ -1,7 +1,7 @@
-import '../../../styles/globals.css';
-import { topicsArray } from '@/assets/servicesData/services';
-import { notFound } from 'next/navigation';
-import Link from 'next/link';
+import "../../../styles/globals.css";
+import { topicsArray } from "@/assets/servicesData/services";
+import { notFound } from "next/navigation";
+import Link from "next/link";
 
 export const dynamicParams = true;
 
@@ -12,52 +12,46 @@ export async function generateStaticParams() {
 }
 
 export const getSingleLesson = async (id: any) => {
-    const data = topicsArray[id];
-  
-    if (!data) {
-      notFound();
-    }
-  
-    return data;
-  };
+  const data = topicsArray[id];
+
+  if (!data) {
+    notFound();
+  }
+
+  return data;
+};
 
 export default async function SideNav({
   children,
-  params // will be a page or nested layout
+  params, // will be a page or nested layout
 }: {
-  children: React.ReactNode,
+  children: React.ReactNode;
   params: { subjectId: string };
 }) {
-
-    const subject = await getSingleLesson(params.subjectId);
+  const subject = await getSingleLesson(params.subjectId);
 
   return (
-    <section className='flex'>
+    <section className="flex">
       {/* Include shared UI here e.g. a header or sidebar */}
 
-
       <div>
-      <h1>Subject: {params.subjectId} - {subject.subject} </h1>
-      {/* <p>Select a topic to explore:</p> */}
+        <h1>
+          Subject: {params.subjectId} - {subject.subject}{" "}
+        </h1>
 
-      {subject.content.map(topic => (
-        <div key={topic.id}>
-          <h1 className='font-bold'>{topic.topic}</h1>
-          <Link href={`/teas/${params.subjectId}/topics/${topic.id}/${topic.id}`}>{topic.subtopic}</Link>
-        </div>
-      ))}
+        {subject.content.map((topic) => (
+          <div key={topic.id}>
+            <h1 className="font-bold">{topic.topic}</h1>
+            <Link
+              href={`/teas/${params.subjectId}/topics/${topic.id}/${topic.id}`}
+            >
+              {topic.subtopic}
+            </Link>
+          </div>
+        ))}
+      </div>
 
-      {/* {subject.content.map(topic => (
-        <ul key={topic.id}>
-        <li>
-          <Link href={`/teas/${params.subjectId}/topics/${topic.id}`}>{topic.topic}</Link>
-        </li>
-        </ul>
-      ))} */}
-     
-    </div>
-      
       {children}
     </section>
-  )
+  );
 }

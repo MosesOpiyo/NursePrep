bb32f094a254a30159712e75d06b52d8b0e099d9
unknown <githinjipete21@gmail.com>
2024-09-27 17:58:07 +0300

    edit hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import hero from "../../assets/hero4.jpg";
 import Image from "next/image";
@@ -12,27 +14,58 @@ import guides from "../../assets/guides.jpg";
 import performance from "../../assets/performance.jpg";
 import community from "../../assets/community.jpg";
 import exam from "../../assets/examday.jpg";
+import { useEffect, useRef, useState } from "react";
+import styles from './ScrollAnimation.module.css';
 
 const Hero = () => {
+
+  // Intersection Observer
+  
+  const boxRef = useRef<HTMLDivElement | null>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const boxElement = boxRef.current;
+
+    const handleScroll = () => {
+      if (boxElement) {
+        const rect = boxElement.getBoundingClientRect();
+        const isInViewport = rect.top <= window.innerHeight && rect.bottom >= 0;
+        setIsVisible(isInViewport);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    
+    // Initial check in case the element is already visible on load
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+
+
   return (
       <section className="hero-container grid items-center justify-center w-full relative">
-        <div className="hero-text sm:pt-32 text-white p sm:px-16 h-full w-full flex flex-col gap-10 items-center justify-center">
+        <div className={`hero-text sm:pt-32 text-white p sm:px-16 h-full w-full flex flex-col gap-10 items-center justify-center lg:w-11/12 lg:mx-auto ${styles.box} ${isVisible ? styles.visible : ""}`} ref={boxRef}>
 
           {/* HERO HEADER TEXT */}
-          <h1 className="hero-header relative text-center sm:text-6xl text-5xl">
+          <h1 className="hero-header relative text-center sm:text-6xl md:text-7xl text-5xl font-extralight lg:w-4/5">
             Ace your nursing school entrance exams. <span>Guaranteed.</span>
           </h1>
 
           {/* HERO PARAGRAPH */}
-          <p className="hero-paragraph text-center">
+          <p className="hero-paragraph text-center font-extralight">
             Pass your entrance tests with our accurate practice questions and
             detailed answer explanations.
           </p>
 
           {/* HERO BUTTONS */}
           <div className="action-btn flex gap-4">
-            <Button>GET STARTED NOW</Button>
-            <Button>BROWSE FREE TESTS</Button>
+            <Button className="p-5">GET STARTED NOW</Button>
+            <Button className="p-5">BROWSE FREE TESTS</Button>
           </div>
         </div>
 
